0133fe73770809d836cc349c7d7dd291475788c9
Abderrahmane BENAISSA <76059937+Abdulrahman-02@users.noreply.github.com>
2023-08-13 21:19:47 +0000

    serve data in templates

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"html/template"
+)
+
+type film struct {
+	Title string
+	Director string
+}
+
+func main(){
+	fmt.Println("Go-htmx")
+	
+	handler1 := func(w http.ResponseWriter, req *http.Request){
+		// Define the data
+		films := map[string][]film{
+			"Films": {
+				{Title: "Casablanca", Director: "Michael Curtiz"},
+				{Title: "Cool Hand Luke", Director: "Stuart Rosenberg"},
+				{Title: "Bullitt", Director: "Peter Yates"},
+			},
+			}
+
+		// Render the template
+		tmpl := template.Must(template.ParseFiles("index.htm"))
+		tmpl.Execute(w, films)
+
+		// io.WriteString(w,"Go-htmx \n")
+		// io.WriteString(w,r.Method)
+	}
+	http.HandleFunc("/",handler1)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
